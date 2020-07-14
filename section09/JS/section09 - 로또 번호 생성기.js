52f8f344a30eeccae23ea325a3ce14d967f71521
YongjunKang <yj.kang@zipida.com>
2020-07-14 13:07:14 +0900

    Fix section09
    - Fix section09.js

--- section09/JS/section09 - 로또 번호 생성기.js
+++ section09/JS/section09 - 로또 번호 생성기.js
@@ -8,10 +8,10 @@ let {count, maxNumber} = SETTING;
 let lottoSet = new Set();
 
 function getRandomNumber(maxNumber) {
-    let num = Math.floor(Math.random() * maxNumber) + 1;
-    if(num <= maxNumber) {
-        lottoSet.add(num);
-    }
+    let num = Math.floor(Math.random() * maxNumber) + 1; // 1~45만 뽑는다.
+        if(!lottoSet.has(num)) {
+            lottoSet.add(num);
+        }
 }
 
 for(let i=0; i<count; i++) {
